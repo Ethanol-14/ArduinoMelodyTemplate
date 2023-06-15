5cf72e6f3cd4b10aff6580446afa34671dc56f91
Ethanol-14 <69091472+Ethanol-14@users.noreply.github.com>
2023-06-14 20:45:07 -0700

    Add files via upload

--- python midi reader.py
+++ python midi reader.py
@@ -5,7 +5,7 @@ mid = mido.MidiFile("pkmnrby.mid", clip = True)
 for tracks in mid.tracks:
     print(tracks)
 
-for notesA in mid.tracks[1][:4000]:
+for notesA in mid.tracks[2][:4000]:
     print(notesA)
 
 print("done")
